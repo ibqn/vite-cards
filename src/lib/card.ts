89220931c02bb1bd99c929aab693bf376c866e84
Evgeny Bobkin <evgeny.bobkin@europace.de>
2025-04-13 18:09:59 +0200

    feat: initialize Vite template with React and Tailwind CSS
    - Added Vite configuration for React and Tailwind CSS.
    - Created main application structure with App component.
    - Implemented card utility functions for card game logic.
    - Included SVG assets for Vite and React logos.
    - Set up TypeScript configuration for app and node environments.
    - Added utility function for merging class names.
    - Created initial styles with Tailwind CSS.
    - Updated README with project description.

---
+++ src/lib/card.ts
@@ -0,0 +1,46 @@
+export const Suite = {
+  hearts: 0,
+  diamonds: 1,
+  clubs: 2,
+  spades: 3,
+} as const
+
+export type Suite = (typeof Suite)[keyof typeof Suite]
+
+export const Rank = {
+  six: 0,
+  seven: 1,
+  eight: 2,
+  nine: 3,
+  ten: 4,
+  jack: 5,
+  queen: 6,
+  king: 7,
+  ace: 8,
+} as const
+
+export type Rank = (typeof Rank)[keyof typeof Rank]
+
+export type Card = number
+
+const SUITE_BIN_SIZE = 2
+const RANK_BIN_SIZE = 4
+const SUITE_MASK = (1 << SUITE_BIN_SIZE) - 1
+const RANK_MASK = (1 << RANK_BIN_SIZE) - 1
+
+export const createCard = (suite: Suite, rank: Rank): Card => {
+  return ((rank << SUITE_BIN_SIZE) & RANK_MASK) | (suite & SUITE_MASK)
+}
+export const getSuite = (card: Card): Suite => {
+  return (card & SUITE_MASK) as Suite
+}
+
+export const getRank = (card: Card): Rank => {
+  return ((card >> SUITE_BIN_SIZE) & RANK_MASK) as Rank
+}
+
+export const getCardString = (card: Card): string => {
+  const rank = getRank(card)
+  const suite = getSuite(card)
+  return `${rank}-${suite}`
+}
